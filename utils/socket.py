9f9f05076332492f8c1c0e347e97c3b2922b185e
pewt1403 <p_ngamboontham@hotmail.com>
2021-05-17 00:00:19 +0700

    Delete old model

--- utils/socket.py
+++
@@ -1,9 +0,0 @@
-
-import socket
-def init_udp_socket():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return sock
-
-def send_data(sock, msg, IP, PORT):
-    msg = bytes("{}".format(msg), encoding="utf8")
-    sock.sendto(msg, (IP, PORT))
